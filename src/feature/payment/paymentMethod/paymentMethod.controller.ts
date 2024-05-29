26d7da80d1ee4c2aa66cfedbe8150a807642812c
VuKhoiGVM <nguyenvukhoi150402@gmail.com>
2024-05-29 13:39:17 +0700

    fix: fix find all method payment

--- src/feature/payment/paymentMethod/paymentMethod.controller.ts
+++ src/feature/payment/paymentMethod/paymentMethod.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Delete, Get, Inject, Param, Post, Query, Req, UseGuards } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Inject,
+    Param,
+    Post,
+    Query,
+    Req,
+    UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AccessTokenGuard } from 'src/common/guards/token/accessToken.guard';
 import { RolesGuard } from 'src/common/guards/role/role.guard';
@@ -12,10 +23,19 @@ import {
     ApiParamExamples,
     ApiResponseExample,
 } from 'src/common/decorator/swagger.decorator';
-import {PaymentPaymentMethodService} from './paymentMethod.service';
-import {CreatePaymentMethod, CreatePaymentMethodRequestDTO, DeletePaymentMethod, DeletePaymentMethodRequestDTO, GetPaymentMethod, GetPaymentMethodRequestDTO, ListPaymentMethodRequestDTO, UpdatePaymentMethod, UpdatePaymentMethodRequestDTO} from './paymentMethod.dto';
-import {GetPaymentMethodRequest} from 'src/proto_build/payment/paymentMethod/GetPaymentMethodRequest';
- 
+import { PaymentPaymentMethodService } from './paymentMethod.service';
+import {
+    CreatePaymentMethod,
+    CreatePaymentMethodRequestDTO,
+    DeletePaymentMethod,
+    DeletePaymentMethodRequestDTO,
+    GetPaymentMethod,
+    GetPaymentMethodRequestDTO,
+    ListPaymentMethodRequestDTO,
+    UpdatePaymentMethod,
+    UpdatePaymentMethodRequestDTO,
+} from './paymentMethod.dto';
+import { GetPaymentMethodRequest } from 'src/proto_build/payment/paymentMethod/GetPaymentMethodRequest';
 
 @Controller('payment/method')
 @ApiTags('payment/method')
@@ -41,16 +61,8 @@ Create a Payment Method within a domain using an access token. This operation is
 - **status** is optional
 `,
     })
-    @ApiBodyExample(CreatePaymentMethod, { 
-    })
-    @ApiResponseExample(
-        'create',
-        'create Policy and Term',
-        {
-             
-        },
-        '/api/tenant/policy/create',
-    )
+    @ApiBodyExample(CreatePaymentMethod, {})
+    @ApiResponseExample('create', 'create Policy and Term', {}, '/api/tenant/policy/create')
     @ApiErrorResponses('/api/tenant/policy/create', '/api/tenant/policy/create', {
         badRequest: {
             summary: 'Validation Error',
@@ -108,106 +120,6 @@ Create a Payment Method within a domain using an access token. This operation is
             user: userData,
             ...data,
         } as CreatePaymentMethodRequestDTO);
-    }
-
-    @Get('find/:id')
-    @UseGuards(AccessTokenGuard)
-    @ApiBearerAuth('JWT-access-token-user')
-    @ApiBearerAuth('JWT-access-token-tenant')
-    @ApiEndpoint({
-        summary: `Find one Policy and Term by TenantID`,
-        details: `
-## Description
-Find a Policy and Term by TenantId within a domain using an access token.
-## Requirements
-- **Access Token**: Must provide a valid access token.
-`,
-    })
-    @ApiParamExamples([
-        {
-            name: 'tenantId',
-            description: 'ID of Tenant in DB',
-            example: 'd4d98d4c-d2f4-4d91-a6e7-2555715ce144',
-            required: true,
-        },
-    ])
-    @ApiResponseExample(
-        'read',
-        'find a Policy and Term by tenantId',
-        {
-            policyAndTerm: {
-                id: '4ce1d0b3-7904-4ead-bcd9-97ceb351116a',
-                tenantId: 'd4d98d4c-d2f4-4d91-a6e7-2555715ce144',
-                policy: 'policy gi do ',
-                term: 'term gi do',
-                createdAt: '2024-05-21T18:33:07.638Z',
-                updatedAt: '2024-05-21T18:33:07.638Z',
-            },
-        },
-        '/api/tenant/policy/find/d4d98d4c-d2f4-4d91-a6e7-2555715ce144',
-    )
-    @ApiErrorResponses(
-        '/api/tenant/policy/find/:tenantId',
-        '/api/tenant/policy/find/d4d98d4c-d2f4-4d91-a6e7-2555715ce144',
-        {
-            badRequest: {
-                summary: 'Validation Error',
-                detail: 'tenantId must be a UUID',
-            },
-
-            unauthorized: [
-                {
-                    key: 'token_not_verified',
-                    summary: 'Token not verified',
-                    detail: 'Unauthorized',
-                    error: null,
-                },
-                {
-                    key: 'token_not_found',
-                    summary: 'Token not found',
-                    detail: 'Access Token not found',
-                    error: 'Unauthorized',
-                },
-                {
-                    key: 'unauthorized_role',
-                    summary: 'Role not verified',
-                    detail: 'Unauthorized Role',
-                    error: 'Unauthorized',
-                },
-                {
-                    key: 'not_found',
-                    summary: 'Policy and term not found',
-                    detail: 'Policy and term not found',
-                    error: 'Unauthorized',
-                },
-            ],
-            forbidden: [
-                {
-                    key: 'forbidden_resource',
-                    summary: 'Forbidden resource',
-                    detail: 'Forbidden resource',
-                },
-            ],
-        },
-    )
-    async findOnePaymentMethod(
-        @Req() req: Request,
-        @Query() data: GetPaymentMethod,
-    ) {
-        // console.log(data);
-        const payloadToken = req['user'];
-        // const header = req.headers;
-        const userData = {
-            email: payloadToken.email,
-            domain: payloadToken.domain,
-            role: payloadToken.role,
-            accessToken: payloadToken.accessToken,
-        } as UserDto;
-        // console.log(userData, dataCategory)
-        return await this.paymentPaymentMethodService.getPaymentMethod({
-            user: userData,
-            ...data,
-        } as GetPaymentMethodRequestDTO);
     }
 
     @Get('find/all')
@@ -290,10 +202,104 @@ Find a Policy and Term by TenantId within a domain using an access token.
             ],
         },
     )
-    async findAllPaymentMethod(
-        @Req() req: Request,
-        
-    ) {
+    async findAllPaymentMethod(@Req() req: Request) {
+        // console.log(data);
+        const payloadToken = req['user'];
+        // const header = req.headers;
+        const userData = {
+            email: payloadToken.email,
+            domain: payloadToken.domain,
+            role: payloadToken.role,
+            accessToken: payloadToken.accessToken,
+        } as UserDto;
+        console.log('abc');
+        // console.log(userData, dataCategory)
+        return await this.paymentPaymentMethodService.listPaymentMethod({
+            user: userData,
+        } as ListPaymentMethodRequestDTO);
+    }
+
+    @Get('find/:id')
+    @UseGuards(AccessTokenGuard)
+    @ApiBearerAuth('JWT-access-token-user')
+    @ApiBearerAuth('JWT-access-token-tenant')
+    @ApiEndpoint({
+        summary: `Find one Policy and Term by TenantID`,
+        details: `
+## Description
+Find a Policy and Term by TenantId within a domain using an access token.
+## Requirements
+- **Access Token**: Must provide a valid access token.
+`,
+    })
+    @ApiParamExamples([
+        {
+            name: 'tenantId',
+            description: 'ID of Tenant in DB',
+            example: 'd4d98d4c-d2f4-4d91-a6e7-2555715ce144',
+            required: true,
+        },
+    ])
+    @ApiResponseExample(
+        'read',
+        'find a Policy and Term by tenantId',
+        {
+            policyAndTerm: {
+                id: '4ce1d0b3-7904-4ead-bcd9-97ceb351116a',
+                tenantId: 'd4d98d4c-d2f4-4d91-a6e7-2555715ce144',
+                policy: 'policy gi do ',
+                term: 'term gi do',
+                createdAt: '2024-05-21T18:33:07.638Z',
+                updatedAt: '2024-05-21T18:33:07.638Z',
+            },
+        },
+        '/api/tenant/policy/find/d4d98d4c-d2f4-4d91-a6e7-2555715ce144',
+    )
+    @ApiErrorResponses(
+        '/api/tenant/policy/find/:tenantId',
+        '/api/tenant/policy/find/d4d98d4c-d2f4-4d91-a6e7-2555715ce144',
+        {
+            badRequest: {
+                summary: 'Validation Error',
+                detail: 'tenantId must be a UUID',
+            },
+
+            unauthorized: [
+                {
+                    key: 'token_not_verified',
+                    summary: 'Token not verified',
+                    detail: 'Unauthorized',
+                    error: null,
+                },
+                {
+                    key: 'token_not_found',
+                    summary: 'Token not found',
+                    detail: 'Access Token not found',
+                    error: 'Unauthorized',
+                },
+                {
+                    key: 'unauthorized_role',
+                    summary: 'Role not verified',
+                    detail: 'Unauthorized Role',
+                    error: 'Unauthorized',
+                },
+                {
+                    key: 'not_found',
+                    summary: 'Policy and term not found',
+                    detail: 'Policy and term not found',
+                    error: 'Unauthorized',
+                },
+            ],
+            forbidden: [
+                {
+                    key: 'forbidden_resource',
+                    summary: 'Forbidden resource',
+                    detail: 'Forbidden resource',
+                },
+            ],
+        },
+    )
+    async findOnePaymentMethod(@Req() req: Request, @Query() data: GetPaymentMethod) {
         // console.log(data);
         const payloadToken = req['user'];
         // const header = req.headers;
@@ -304,9 +310,10 @@ Find a Policy and Term by TenantId within a domain using an access token.
             accessToken: payloadToken.accessToken,
         } as UserDto;
         // console.log(userData, dataCategory)
-        return await this.paymentPaymentMethodService.listPaymentMethod({
+        return await this.paymentPaymentMethodService.getPaymentMethod({
             user: userData,
-        } as ListPaymentMethodRequestDTO);
+            ...data,
+        } as GetPaymentMethodRequestDTO);
     }
 
     @Post('update')
@@ -324,8 +331,7 @@ Update a Policy and Term within a domain using an access token. This operation i
 - **Permissions**: Requires tenant-level permissions.
         `,
     })
-    @ApiBodyExample(UpdatePaymentMethod, { 
-    })
+    @ApiBodyExample(UpdatePaymentMethod, {})
     @ApiResponseExample(
         'update',
         'update Policy and Term',
